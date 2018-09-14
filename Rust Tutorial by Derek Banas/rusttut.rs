f46cef4cf090d9badeffce0079bd5eb8758ba305
Dominik Rosser <dominik.rosser@protonmail.ch>
2018-09-14 19:54:09 +0200

    Guess the number, error safe

--- Rust Tutorial by Derek Banas/rusttut.rs
+++ Rust Tutorial by Derek Banas/rusttut.rs
@@ -5,12 +5,109 @@ use std::{i8, i16, i32, i64, u8, u16, u32, u64,
 use std::io::stdin;
 
 fn main() {
-    show_type_sizes();
-    show_age();
-    show_my_name();
-    is_it_true_game();
-    format_some_things();
-    basic_calculations();
+    // show_type_sizes();
+    // show_age();
+    // show_my_name();
+    // is_it_true_game();
+    // format_some_things();
+    // basic_calculations();
+    // looping();
+    // stringstest1();
+    inputtest1();
+    // TODO https://youtu.be/U1EFgCNLDB8?t=25m47s
+}
+
+fn inputtest1() {
+    'outer: loop {
+        let number: i32 = 10;
+        println!("Pick a number");
+
+        loop {
+            let mut line = String::new();
+            let input = std::io::stdin().read_line(&mut line);
+
+            let guess: Option<i32> = input.ok().map_or(
+                None,
+                |_| line.trim().parse().ok()
+            );
+
+            match guess {
+                None => println!("Enter a Number"),
+                Some(n) if n == number => {
+                    println!("You guessed it!");
+                    break 'outer;
+                },
+                Some(n) if n < number => println!("Too low"),
+                Some(n) if n > number => println!("Too high"),
+                Some(_) => println!("Error"),
+            }
+        }
+    }
+}
+
+fn stringstest1() {
+    let rand_string = "I am a random string";
+
+    println!("Length: {}", rand_string.len());
+
+    let (first, second) = rand_string.split_at(6);
+    println!("First: {} Second: {}", first, second);
+
+    let count = rand_string.chars().count();
+    let mut chars = rand_string.chars();
+
+    loop {
+        match chars.next() {
+            Some(x) => println!("{}", x),
+            None => break,
+        }
+    }
+
+    let mut iter = rand_string.split_whitespace();
+
+    loop {
+        match iter.next() {
+            Some(word) => println!("Word: {}", word),
+            None => break,
+        }
+    }
+
+    let rand_string_2 = "I am a random string\nThere are other strings like it\nThis string is the best";
+    let mut lines = rand_string_2.lines();
+    loop {
+        match lines.next() {
+            Some(line) => println!("Line: {}", line),
+            None => break,
+        }
+    }
+
+    println!("Find Best: {}", rand_string_2.contains("best"));
+}
+
+fn looping() {
+    let mut x = 1;
+
+    loop {
+        if x % 2 == 0 {
+            println!("{}", x);
+            x += 1;
+            continue;
+        }
+        if x > 10 { break; }
+        x += 1;
+        continue;
+    }
+
+    let mut y = 1;
+
+    while y <= 10 {
+        println!("{}", y);
+        y += 1;
+    }
+
+    for z in 1..11 {
+        println!("FOR: {}", z);
+    }
 }
 
 fn basic_calculations() {
@@ -31,7 +128,11 @@ fn basic_calculations() {
     println!("e ^ 2 = {}", 2f64.exp());
     println!("log(2) = {}", 2f64.ln());
     println!("log10(2) = {}", 2f64.log10());
-    // TODO https://youtu.be/U1EFgCNLDB8?t=10m8s
+    println!("90 to Radians = {}", 90.0f64.to_radians());
+    println!("PI to Degrees = {}", 3.14f64.to_degrees());
+    println!("Max 4, 5 = {}", 4f64.max(5f64));
+    println!("Min 4, 5 = {}", 4f64.min(5f64));
+    println!("Sin 3.14 = {}", 3.14f64.sin());
 }
 
 fn format_some_things() {
